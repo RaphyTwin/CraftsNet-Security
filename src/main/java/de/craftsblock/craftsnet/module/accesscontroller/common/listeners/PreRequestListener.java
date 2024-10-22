741dfc0dd04f89e4c3bbc13564c48fb80da440ad
Philipp <62473021+CrAfTsArMy@users.noreply.github.com>
2024-10-22 10:06:12 +0200

    [ADD] Request listener for the processing of a request

---
+++ src/main/java/de/craftsblock/craftsnet/module/accesscontroller/common/listeners/PreRequestListener.java
@@ -0,0 +1,46 @@
+package de.craftsblock.craftsnet.module.accesscontroller.common.listeners;
+
+import de.craftsblock.craftscore.event.EventHandler;
+import de.craftsblock.craftscore.event.EventPriority;
+import de.craftsblock.craftscore.event.ListenerAdapter;
+import de.craftsblock.craftscore.json.Json;
+import de.craftsblock.craftsnet.api.http.Exchange;
+import de.craftsblock.craftsnet.events.requests.PreRequestEvent;
+import de.craftsblock.craftsnet.module.accesscontroller.AccessController;
+import de.craftsblock.craftsnet.module.accesscontroller.api.auth.AuthResult;
+import de.craftsblock.craftsnet.module.accesscontroller.api.auth.chains.AuthChain;
+
+import java.io.IOException;
+
+/**
+ * The PreRequestListener class listens for pre-request events and processes
+ * authentication chains to determine if an incoming request should be allowed.
+ */
+public class PreRequestListener implements ListenerAdapter {
+
+    /**
+     * Handles the {@link PreRequestEvent}. This method is triggered when a pre-request
+     * event occurs and processes the authentication chains.
+     *
+     * @param event The {@link PreRequestEvent} containing information about the request.
+     * @throws IOException If an error occurs while processing the request or response.
+     */
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void handlePreRequest(PreRequestEvent event) throws IOException {
+        Exchange exchange = event.getExchange();
+
+        // Iterate through each authentication chain
+        for (AuthChain chain : AccessController.getAuthChainManager()) {
+            // Authenticate the incoming request using the current chain
+            AuthResult result = chain.authenticate(exchange.request());
+
+            // Check if the authentication was cancelled
+            if (result.isCancelled()) {
+                event.setCancelled(true); // Cancel the event
+                // Send an error response back to the client
+                exchange.response().print(Json.empty().set("error", result.getCancelReason()));
+            }
+        }
+    }
+
+}
